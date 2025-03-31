9a181d4e717b22ae44849e0814fbbede0f32d4e9
Liumingxun <liumingxun@yeah.net>
2025-03-31 12:17:31 +0000

    using client calling tools with AI

--- packages/client/index.ts
+++ packages/client/index.ts
@@ -1,7 +1,13 @@
 /* eslint-disable no-console */
 /* eslint-disable antfu/no-top-level-await */
+import type O from 'openai'
+import type { ChatCompletionTool } from 'openai/resources/chat'
+import process, { loadEnvFile } from 'node:process'
 import { Client } from '@modelcontextprotocol/sdk/client/index'
 import { StdioClientTransport } from '@modelcontextprotocol/sdk/client/stdio'
+import { OpenAI } from 'openai'
+
+loadEnvFile('.env')
 
 const transport = new StdioClientTransport({
   command: 'pnpm',
@@ -29,3 +35,61 @@ mcpClient.callTool({
 }).catch((err) => {
   console.error('Error calling tool:', err)
 })
+
+// using model
+const messages: O.Chat.Completions.ChatCompletionMessageParam[] = [
+  {
+    role: 'system',
+    content: 'you\'re a echo bot, do nothing except echo back what you receive with you tool',
+  },
+  {
+    role: 'user',
+    content: '我不是人工智能',
+  },
+]
+
+const client = new OpenAI({
+  baseURL: 'https://api.deepseek.com',
+  apiKey: `${process.env.API_KEY}`,
+})
+
+async function sendMessages(messages: O.Chat.Completions.ChatCompletionMessageParam[]) {
+  const response = await client.chat.completions.create({
+    model: 'deepseek-chat',
+    messages,
+    tools: (await mcpClient.listTools()).tools.map<ChatCompletionTool>(tool => ({
+      function: {
+        name: tool.name,
+        parameters: tool.inputSchema,
+      },
+      type: 'function',
+    })),
+  })
+  return response.choices[0].message
+}
+
+const res = await sendMessages(messages)
+
+messages.push(res)
+const tool_calls = res.tool_calls
+
+if (tool_calls) {
+  for (const tool of tool_calls) {
+    const call = await mcpClient.callTool({
+      name: tool.function.name,
+      arguments: JSON.parse(tool.function.arguments),
+    })
+    console.log(call)
+    messages.push({
+      role: 'tool',
+      tool_call_id: tool.id,
+      content: JSON.stringify((call.content as O.Chat.Completions.ChatCompletionContentPartText[])[0].text),
+    })
+  }
+}
+
+console.log(messages)
+
+const res2 = await sendMessages(messages)
+
+console.log(res2.content)
